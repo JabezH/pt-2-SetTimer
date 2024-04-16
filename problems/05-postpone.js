b032b3bfefc869d849ee59d2a0a1bc4c83197b9a
tylerwebster <webstertyler81@yahoo.com>
2024-04-16 19:22:57 -0400

    05 finished

--- problems/05-postpone.js
+++ problems/05-postpone.js
@@ -1,13 +1,4 @@
 /***********************************************************************
-Write a function, `postpone`, that accepts a callback and a delay in 
-milliseconds as arguments. `postpone` should return a new function. When
-the returned function is called, it should invoke the callback after the 
-given delay.
-
-Hint: use closures and setTimeout to your advantage
-
-In addition to Mocha, we recommend that you test your code manually using 
-node with the examples below.
 
 Examples
 
@@ -21,10 +12,29 @@ const slowerGoodbye = postpone(sayGoodbye, 1750);
 console.log(slowerGoodbye); // [Function]
 slowerGoodbye(); // prints 'bye' after 1750 ms
 ***********************************************************************/
+// Write a function, `postpone`, that accepts a callback and a delay in
+// milliseconds as arguments. `postpone` should return a new function. When
+// the returned function is called, it should invoke the callback after the
+// given delay.
 
+// Hint: use closures and setTimeout to your advantage
+
+// In addition to Mocha, we recommend that you test your code manually using
+// node with the examples below.
 function postpone(cb, delay) {
-  // Your code here 
-}
+  //  console.log(cb, delay)
+  return function () {
+       setTimeout(cb, delay);
+    }
+
+  }
+
+
+
+
+console.log(postpone(function () {
+  console.log('hi')
+}, 2000)());
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
